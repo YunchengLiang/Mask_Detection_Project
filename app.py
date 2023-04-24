29fe0b608edf8fbf6a144f0ad243359bdd68ff6b
YunchengLiang <yuncheng.liang@rci.rogers.ca>
2023-04-24 11:16:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -158,4 +158,4 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0', threaded=True)
