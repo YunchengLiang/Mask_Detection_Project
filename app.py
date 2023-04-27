167297ac865e9b01db23f0260ebb8a03dd658bdf
YunchengLiang <yuncheng.liang@rci.rogers.ca>
2023-04-26 22:08:09 -0400

    celery redis try implementatiopn for async

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash, redirect, url_for, send_file, Response
+from flask import Flask, request, render_template, flash, redirect, url_for, send_file, Response, jsonify
 from werkzeug.utils import secure_filename
 from datetime import datetime
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
@@ -8,15 +8,81 @@ import numpy as np
 import argparse
 import cv2
 import os
+from celery import Celery
+from redis import Redis, ConnectionPool
+
+redis_config = {
+    'host': 'localhost',
+    'port': 6379,
+    'db': 0,
+    'password': ''
+}
+redis_pool= ConnectionPool(**redis_config)
+redis=Redis(connection_pool=redis_pool)
+
 
 app = Flask(__name__)
+
+def make_celery(app):
+    celery = Celery(app.name)
+    celery.config_from_object('celeryconfig') 
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self,*args,**kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self,*args,**kwargs)
+    celery.Task=ContextTask
+    return celery
+
+celery= make_celery(app)
 
 prototxtPath= os.path.join("face_detector","deploy.prototxt")
 weightsPath= os.path.join("face_detector","res10_300x300_ssd_iter_140000.caffemodel")
 net= cv2.dnn.readNet(prototxtPath, weightsPath)
 model= load_model('mask_detector.model')
 
-def detect_and_predict_mask(frame,faceNet,maskModel):
+@celery.task(bind=True)
+def detect_from_image(self, image, net, model, filename):
+    (h,w)= image.shape[:2]
+    blob= cv2.dnn.blobFromImage(image,1.0,(300,300),(104.0,177.0,123.0))#normalization
+
+    #computing face detection
+    net.setInput(blob)
+    detections= net.forward()
+    for i in range(0,detections.shape[2]):
+        confidence= detections[0,0,i,2]
+        if confidence>0.5:
+            box= detections[0,0,i,3:7]*np.array([w,h,w,h])
+            (start_X,start_Y, end_X, end_Y)= box.astype('int')
+            (start_X,start_Y)= (max(0,start_X),max(0,start_Y))
+            (end_X,end_Y)= (min(w-1,end_X),min(h-1,end_Y))
+            face= image[start_Y:end_Y, start_X:end_X]
+            face= cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
+            face= cv2.resize(face, (224,224))
+            face= img_to_array(face)
+            face= preprocess_input(face)
+            face= np.expand_dims(face, axis=0)
+
+            (with_mask,without_mask)= model.predict(face)[0]
+            if with_mask>without_mask:
+                label="Mask"
+                color= (0,255,0) 
+            else:
+                label="No Mask"
+                color= (0,0,255)
+
+            label="{}:{:.2f}%".format(label, max(with_mask,without_mask)*100) 
+            cv2.putText(image,label,(start_X,start_Y-10),cv2.FONT_HERSHEY_SIMPLEX,0.45,color,2)
+            cv2.rectangle(image, (start_X,start_Y),(end_X,end_Y),color,2)
+    outputpath=os.path.join("static/online_detector_results",filename)
+    cv2.imwrite(outputpath, image)
+    return 
+
+
+
+def detect_and_predict_mask(self,frame,faceNet,maskModel):
     (h,w)= frame.shape[:2]
     blob= cv2.dnn.blobFromImage(frame,1.0,(300,300),(104.0,177.0,123.0))#normalization
     faceNet.setInput(blob)
@@ -67,40 +133,7 @@ def predict():
         filename = secure_filename(imagefile.filename)
         imagefile.save(filesavepath)
         image= cv2.imread(filesavepath)
-        (h,w)= image.shape[:2]
-
-        blob= cv2.dnn.blobFromImage(image,1.0,(300,300),(104.0,177.0,123.0))#normalization
-
-        #computing face detection
-        net.setInput(blob)
-        detections= net.forward()
-        for i in range(0,detections.shape[2]):
-            confidence= detections[0,0,i,2]
-            if confidence>0.5:
-                box= detections[0,0,i,3:7]*np.array([w,h,w,h])
-                (start_X,start_Y, end_X, end_Y)= box.astype('int')
-                (start_X,start_Y)= (max(0,start_X),max(0,start_Y))
-                (end_X,end_Y)= (min(w-1,end_X),min(h-1,end_Y))
-                face= image[start_Y:end_Y, start_X:end_X]
-                face= cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
-                face= cv2.resize(face, (224,224))
-                face= img_to_array(face)
-                face= preprocess_input(face)
-                face= np.expand_dims(face, axis=0)
-
-                (with_mask,without_mask)= model.predict(face)[0]
-                if with_mask>without_mask:
-                    label="Mask"
-                    color= (0,255,0) 
-                else:
-                    label="No Mask"
-                    color= (0,0,255)
-
-                label="{}:{:.2f}%".format(label, max(with_mask,without_mask)*100) 
-                cv2.putText(image,label,(start_X,start_Y-10),cv2.FONT_HERSHEY_SIMPLEX,0.45,color,2)
-                cv2.rectangle(image, (start_X,start_Y),(end_X,end_Y),color,2)
-        outputpath=os.path.join("static/online_detector_results",filename)
-        cv2.imwrite(outputpath, image)
+        detect_from_image.delay(image, net, model, filename) 
         return render_template('homepage.html', outputImage=filename,error=error)
     else:
         error=True
